9c6bf11df318718b1c825f5fa44b0e7daee5ffbb
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-19 09:21:20 +0545

    feat: Add StudentRegistration page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Hello from '././pages/Hello'; // make sure to import the Hello component
+import StudentRegistration from "./pages/studentregistration";
 
 export default function Home() {
   return (
